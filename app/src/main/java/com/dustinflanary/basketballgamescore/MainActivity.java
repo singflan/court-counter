4fe2d247303b8f56d25e55f053c89b07b3d7f4f5
Dustin Flanary <singflan@gmail.com>
2016-06-27 17:05:03 -0600

    Updated whitespace

--- app/src/main/java/com/dustinflanary/basketballgamescore/MainActivity.java
+++ app/src/main/java/com/dustinflanary/basketballgamescore/MainActivity.java
@@ -4,11 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.view.View;
-
 import junit.framework.Test;
 
 public class MainActivity extends AppCompatActivity {
-
     int scoreTeam1 = 0;
     int scoreTeam2 = 0;
 
